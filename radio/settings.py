16082d27cf225a5fdd7fe956381f216ae22ec859
Anghelo <agiron_1907@hotmail.com>
2018-04-28 17:38:29 -0500

    First Commit

--- radio/settings.py
+++ radio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm5w%&ld##z*x+8=tm-cm-vf_bat2qpf=67m(2hhaxyfy*&3p75'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
