3bc8215dd3ae9caefb4f04d2418fae911752d984
Anghelo <agiron_1907@hotmail.com>
2018-04-28 17:42:34 -0500

    First commit

--- radio/settings.py
+++ radio/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = ''
 
-STATIC_ROOT= '/static/'
+STATIC_ROOT= ''
 
 
 # Configure Django App for Heroku.
